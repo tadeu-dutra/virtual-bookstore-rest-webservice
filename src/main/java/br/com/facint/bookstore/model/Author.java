ea4ee85a8f38e56e5cdce20d615f3e8df1053975
Tadeu Augusto Dutra Pinto <tadeu.dutra@globant.com>
2026-01-01 19:14:50 -0300

    dev: add jaxb mapping to Author model class

--- src/main/java/br/com/facint/bookstore/model/Author.java
+++ src/main/java/br/com/facint/bookstore/model/Author.java
@@ -1,9 +1,21 @@
 package br.com.facint.bookstore.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 import lombok.Data;
 
 @Data
+@XmlRootElement(name = "author")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Author {
+
+    @XmlAttribute
     private Long id;
+
+    @XmlElement
     private String name;
 }
