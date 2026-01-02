7462b0fe227d7f92306b3c82575cd87291186c4f
Tadeu Augusto Dutra Pinto <tadeu.dutra@globant.com>
2026-01-02 03:40:43 -0300

    fix: add getters and setters to model classes

--- src/main/java/br/com/facint/bookstore/model/Author.java
+++ src/main/java/br/com/facint/bookstore/model/Author.java
@@ -22,4 +22,20 @@ public class Author {
         this.id = id;
         this.name = name;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/br/com/facint/bookstore/model/Book.java
+++ src/main/java/br/com/facint/bookstore/model/Book.java
@@ -3,6 +3,7 @@ package br.com.facint.bookstore.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.json.bind.annotation.JsonbTransient;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
@@ -29,6 +30,7 @@ public class Book {
     private Double price;
 
     @XmlElement
+    @JsonbTransient
     private List<Author> author = new ArrayList<Author>();
 
     public Book() {}
@@ -42,11 +44,51 @@ public class Book {
         this.author = author;
     }
 
-    public Long getId() {
+        public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getIsbn() {
         return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public List<Author> getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(List<Author> author) {
+        this.author = author;
     }
 }
