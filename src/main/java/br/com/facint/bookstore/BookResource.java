0fa991abd9928e0ad956540b4b002e9152ba2809
Tadeu Augusto Dutra Pinto <tadeu.dutra@globant.com>
2026-01-02 04:21:12 -0300

    feat: add updateBook to BookResource

--- src/main/java/br/com/facint/bookstore/BookResource.java
+++ src/main/java/br/com/facint/bookstore/BookResource.java
@@ -3,6 +3,7 @@ package br.com.facint.bookstore;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -57,5 +58,23 @@ public class BookResource {
             .build(book.getIsbn());
 
         return Response.created(uriLocation).entity(book).build();
+    }
+
+    @PUT
+    @PathParam("/{isbn}")
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Response updateBook(@PathParam("isbn") String isbn, Book book) {
+        try {
+            Book inStockBook = bookRepository.getBookByIsbn(isbn);
+            inStockBook.setAuthor(book.getAuthor());
+            inStockBook.setGenre(book.getGenre());
+            inStockBook.setPrice(book.getPrice());
+            inStockBook.setTitle(book.getTitle());
+            bookRepository.updateBook(inStockBook);
+        } catch (BookNotFoundException e) {
+            throw new WebApplicationException(Status.NOT_FOUND);
+        }
+
+        return Response.ok().entity(book).build();
     }
 }
